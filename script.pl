32b014918d21bd2f4eb66b65e510d904e8f69484
angelBhim <mlynganuza@gmail.com>
2017-01-20 16:52:43 +0800

    update sql query

--- script.pl
+++ script.pl
@@ -71,13 +71,32 @@ sub get_new_subscriber {
         my $dbh = DBI->connect($dsn, $hosts{$host}{'username'}, $hosts{$host}{'password'});
         
         if ($dbh) {
-            my $sql = "SELECT msisdn, service_id, $host as ac FROM Dmp_Transaction.subscribers a WHERE 1 AND a.`active` = 1 
-                AND DATE(a.`sub_on`) >= (CURDATE() - INTERVAL 1 DAY) AND a.`service_id` IN (
-                    SELECT service_id FROM Dmp_Content.`services` ds 
-                        LEFT JOIN Dmp_Content.`shortcodes` dsc 
-                        ON ds.`shortcode_id` = dsc.`shortcode_id` 
-                        WHERE ds.service_active = 1 AND ds.is_cashagana = 1 
-                        AND dsc.`shortcode_name` = $host) LIMIT 3;";
+            my $sql = "SELECT 
+                        s.msisdn, s.service_id, {$host} AS ac
+                        FROM
+                            Dmp_Transaction.subscribers s
+                                INNER JOIN
+                            (SELECT 
+                                a.msisdn
+                            FROM
+                                Dmp_Transaction.subscribers a
+                            WHERE
+                                1
+                            GROUP BY a.msisdn
+                            HAVING COUNT(a.msisdn) = 1) sa ON s.msisdn = sa.msisdn
+                        WHERE
+                            s.active = 1
+                                AND DATE(s.sub_on) = (CURDATE() - INTERVAL 1 DAY)
+                                AND s.service_id IN (SELECT 
+                                    service_id
+                                FROM
+                                    Dmp_Content.`services` ds
+                                        LEFT JOIN
+                                    Dmp_Content.`shortcodes` dsc ON ds.`shortcode_id` = dsc.`shortcode_id`
+                                WHERE
+                                    ds.service_active = 1
+                                        AND ds.is_cashagana = 1
+                                        AND dsc.`shortcode_name` = {$host});";
 
             my $sth = $dbh->prepare($sql) or die "prepare statement failed: $dbh->errstr()";
             $sth->execute() or die "execution failed: $dbh->errstr()";
